16a9423e106098145353a468d4f0ff0d318b88d3
shahbozolmosov <shahboz.olmosov.dev@gmail.com>
2025-08-26 09:10:54 +0500

    add env example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppConfig, DatabaseConfig } from './config';
+import Joi from 'joi';
+import { appValidationSchema } from './config/app.config';
+import { databaseValidationSchema } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [AppConfig, DatabaseConfig],
+      validationSchema: Joi.object({
+        ...appValidationSchema,
+        ...databaseValidationSchema,
+      }),
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Config
+  const configService = app.get(ConfigService);
+
+  // Secure
+  // Cors
+  // Cookie parser
+  // Global prefix
+  // Versioning
+  // Validation
+  // Swagger
+  // Running
+  const port = process.env.PORT ?? 4040;
+  await app.listen(port);
+
+  console.log(`
+      🚀 Application is running on: http://localhost:${port}
+      📝 API Documentation: http://localhost:${port}/api/docs
+      🌍 Environment: ${configService.get<string>('app.nodenv')}
+    `);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Application failed to start:', error);
+  process.exit(1);
+});
